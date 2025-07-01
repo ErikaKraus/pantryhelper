918c6fce4e7c418f75a7bf8e65071c1ff5d42284
ErikaKraus <r0231723@student.thomasmore.be>
2025-07-01 18:58:01 +0200

    created DAL for categories

---
+++ src/lib/server/dal/categories.ts
@@ -0,0 +1,45 @@
+import 'server-only'
+import {Category } from '@prisma/client'
+import {prismaClient} from '@/lib/server/dal/prismaClient'
+
+export type CreateCategoryParams = {
+    name: string
+    groupId: string
+}
+
+export async function createCategory(params: CreateCategoryParams): Promise<Category> {
+    return prismaClient.category.create({
+        data: {
+            name: params.name,
+            groupId: params.groupId,
+        },
+    })
+}
+
+export async function updateCategory(id: string, params: CreateCategoryParams): Promise<Category> {
+    return prismaClient.category.update({
+        where: {id},
+        data: {
+            name: params.name
+        },
+    })
+}
+
+export async function deleteCategory(id: string): Promise<void> {
+    await prismaClient.category.delete({
+        where: {id},
+    })
+}
+
+export async function getCategoryById(id: string, groupId: string): Promise<Category | null> {
+    return prismaClient.category.findFirst({
+        where: {id, groupId},
+    })
+}
+
+export async function getAllCategories(groupId: string): Promise<Category[]> {
+    return prismaClient.category.findMany({
+        where: {groupId},
+        orderBy: {name: 'asc'}
+    })
+}
