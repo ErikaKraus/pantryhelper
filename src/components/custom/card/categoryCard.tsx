592438624c9d1fceeb58fe63c8a4c04db7dc47ae
ErikaKraus <r0231723@student.thomasmore.be>
2025-07-03 22:16:05 +0200

    created categoryCard and categoriesPage

---
+++ src/components/custom/card/categoryCard.tsx
@@ -0,0 +1,26 @@
+'use client'
+import {FunctionComponent} from 'react'
+import Link from 'next/link'
+import {Card,  CardTitle} from '@/components/ui/card'
+import { motion } from 'framer-motion'
+
+interface CategoryCardProps {
+    title: string
+    link: string
+}
+
+const CategoryCard: FunctionComponent<CategoryCardProps> = ({title, link}) => {
+    return (
+        <motion.div whileHover={{scale: 1.05}}>
+            <Link href={link}>
+                <Card className="min-h-40 w-45 flex items-center justify-center">
+                    <CardTitle className="text-center text-lg sm:text-l md:text-xl lg:text-2xl">
+                       {title}
+                     </CardTitle>
+                </Card>
+            </Link>
+        </motion.div>
+    )
+}
+
+export default CategoryCard
